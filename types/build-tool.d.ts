81ea69f6dfca4ed620b4d1b416356ff193282983
hczhao <haochengzhao@outlook.com>
2025-07-05 20:58:40 +0800

    set env variables

---
+++ types/build-tool.d.ts
@@ -0,0 +1,12 @@
+
+import 'dotenv';
+
+declare module 'dotenv' {
+    export interface DotenvParseOutput {
+        VITE_db_username: string;
+        VITE_host: string;
+        VITE_port: number;
+        VITE_base: string;
+        VITE_api_proxy_domain: string;
+    }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+// @ts-check
+// @tsconfig ./tsconfig.node.json
+
+import { defineConfig } from 'vite'
+import type { CommonServerOptions } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import fs from 'fs';
+import dotenv, { type DotenvParseOutput } from 'dotenv';
+
+export default defineConfig(mode => {
+  let server: CommonServerOptions = {};
+  const envFileName = '.env';
+  const curEnvFileName = envFileName + '.' + mode.mode;
+
+  const envMap:DotenvParseOutput = dotenv.parse(fs.readFileSync(curEnvFileName));
+  if (mode.mode === 'development') {
+    console.log(`On development, trying to load env from ${curEnvFileName}`);
+    server = {
+      port: envMap.VITE_port ? envMap.VITE_port : 3000,
+      host: envMap.VITE_host || 'localhost',
+      proxy: {
+        '/api': {
+          target: envMap.VITE_api_proxy_domain || 'http://localhost:8097',
+        }
+      }
+    }
+  }
+  return {
+    base: '/view',
+    plugins: [vue()],
+    server,
+  }
+});
