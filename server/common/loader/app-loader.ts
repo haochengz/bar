0a5237ac34f8ba4250d8bad64e001d961b5df662
hczhao <haochengzhao@outlook.com>
2025-07-10 04:04:03 +0800

    feat: using dotenv to load secret information from .env file

--- server/common/loader/app-loader.ts
+++ server/common/loader/app-loader.ts
@@ -2,9 +2,12 @@ import Koa from 'koa';
 
 import routerLoader from './router-loader';
 import MiddlewareLoader from './middleware-loader';
+import '../env/env'; // Load environment variables
 
 export default async function laod(app: Koa): Promise<boolean> {
   console.log('Loading application components...');
+
+  // Load environment
 
   // Load middlewares
   await MiddlewareLoader(app);
