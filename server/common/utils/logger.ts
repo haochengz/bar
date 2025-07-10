53c86067cd6220af5f7735522f801c27fd579472
hczhao <haochengzhao@outlook.com>
2025-07-11 01:24:47 +0800

    mis: add comment to logger setup

--- server/common/utils/logger.ts
+++ server/common/utils/logger.ts
@@ -12,6 +12,7 @@ if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir, { recursive: true });
 }
 
+// Level: Trace < Debug < Info < Warn < Error < Fatal
 log4js.configure({
   appenders: {
     // 控制台输出
@@ -41,26 +42,31 @@ log4js.configure({
     errorFilter: {
       type: 'logLevelFilter',
       appender: 'errorFile',
-      level: 'error',
+      level: 'error', // error 以上的日志输出到 errorFile
     },
 
     // 控制台过滤器，只打印 fatal, error, debug
     consoleFilter: {
       type: 'logLevelFilter',
       appender: 'console',
-      level: 'debug',
+      level: 'debug', // debug以上, error以下的日志输出到console
       maxLevel: 'error',
     },
   },
 
   categories: {
     default: {
+      // 日志首先采用infoFile的方式输出, 接着流经errorFilter, 符合条件的输出到errorFile配置,
+      // 接着流经consoleFilter, 符合条件的输出到console配置
       appenders: ['infoFile', 'errorFilter', 'consoleFilter'],
+      // 记录的最低级别
       level: 'debug',
     },
   },
 });
 
+// 这里以后可以创建多个categories, 分别对应调式, 开发, 生产等, 实例化logger对象时, 现在上面的default是
+// 唯一一个category, 有了多个之后就可以传入 category 名称, 比如 log4js.getLogger('development');
 const logger = log4js.getLogger();
 
 // 挂载到全局，现在有类型支持
