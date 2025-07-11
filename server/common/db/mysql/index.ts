5ae1cb333fcd31b90ee5bfa3c573ab53d3da62db
hczhao <haochengzhao@outlook.com>
2025-07-12 06:40:45 +0800

    feat: orm connection pool

--- server/common/db/mysql/index.ts
+++ server/common/db/mysql/index.ts
@@ -8,4 +8,10 @@ export const sequelize = new Sequelize({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   logging: false, // Disable logging for production
+  pool: {
+    max: process.env.DB_POOL_MAX ? Number(process.env.DB_POOL_MAX) : 100, // Maximum number of connections in the pool
+    min: process.env.DB_POOL_MIN ? Number(process.env.DB_POOL_MIN) : 5, // Minimum number of connections in the pool
+    acquire: process.env.DB_POOL_ACQUIRE ? Number(process.env.DB_POOL_ACQUIRE) : 30000, // Maximum time, in milliseconds, that pool will try to get a connection before throwing an error
+    idle: process.env.DB_POOL_IDLE ? Number(process.env.DB_POOL_IDLE) : 10000, // Maximum time, in milliseconds, that a connection can be idle before being released
+  },
 });
